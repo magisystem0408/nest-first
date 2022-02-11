b70d0fe737898923dca5dc1ff6a06f0259535b89
magisystem0408 <masato.matsudo@gmail.com>
2022-02-11 15:45:45 +0900

    crud

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Item} from "./item.model";
+import {ItemStatus} from "./item-status.enum";
 
 @Injectable()
 export class ItemsService {
-    findAll(){
-        return "this is ItemsService"
+    private items: Item[] = [];
+
+    findAll(): Item[] {
+        return this.items
     }
+
+    findById(id: string): Item {
+        return this.items.find((item) => item.id === id)
+    }
+
+    create(item: Item): Item {
+        // 配列に格納
+        this.items.push(item)
+        // 格納したものを返す
+        return item
+    }
+
+    updateStatus(id:string): Item {
+        const item = this.findById(id)
+        item.status =ItemStatus.SOLD_OUT
+        return item
+    }
+
+    delete(id :string):void{
+        this.items =this.items.filter((item) => item.id !==id)
+    }
+
+
+
 }
