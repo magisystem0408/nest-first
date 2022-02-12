2487157c48f4e7016db0533115c764698aa677a9
magisystem0408 <masato.matsudo@gmail.com>
2022-02-12 17:51:12 +0900

    error handling

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,19 @@
+import {IsInt, IsNotEmpty, IsString, MaxLength, Min} from "class-validator";
+import {Type} from "class-transformer";
+
 export class CreateItemDto{
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(40)
     name :string
+
+    @IsInt()
+    @Min(1)
+    // タイプをnumber型と認識させる
+    @Type(()=> Number)
     price :number
+
+    @IsString()
+    @IsNotEmpty()
     description :string
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //バリデート適応
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3001);
 }
 
